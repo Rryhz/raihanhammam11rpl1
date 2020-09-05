73cbcc436198b6136577eaa463f46b1ad90e37e0
Raihan Hammam <reyhanhammamz@gmail.com>
2020-09-05 20:57:23 +0700

    raihan11rpl1

--- app/src/main/java/com/example/raihan11rpl1/ControlClass.java
+++ app/src/main/java/com/example/raihan11rpl1/ControlClass.java
@@ -20,7 +20,7 @@ public class ControlClass extends AppCompatActivity {
             startActivity(new Intent(getApplicationContext(),MainActivity.class));
             finish();
         }else{
-            startActivity(new Intent(getApplicationContext(),Mainmenu.class));
+            startActivity(new Intent(getApplicationContext(),MainMenu.class));
             finish();
 
         }
